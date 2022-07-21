27983b33fb99f692a9b8b8408325a33656b960c0
alperylmz1 <107919253+alperylmz1@users.noreply.github.com>
2022-07-21 08:07:59 +0300

    Add files via upload
    User Controller eklendi.

---
+++ src/main/java/com/alper/springbootsmaros/controller/UserController.java
@@ -0,0 +1,115 @@
+package com.alper.springbootsmaros.controller;
+
+
+import com.alper.springbootsmaros.model.Customer;
+
+import com.alper.springbootsmaros.model.Timesheet;
+import com.alper.springbootsmaros.model.User;
+import com.alper.springbootsmaros.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/maroapi")
+public class UserController {
+
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> findAll() {
+        try {
+            List<User> users = userRepository.findAll();
+            if (users.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(users, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        try {
+            User _user = userRepository.save(new User(user.getEmail(), user.getName(), user.getStatus(), user.getRole(), user.getCreateDate(), user.getCreateUser()));
+            return new ResponseEntity<>(_user, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/users/user/{email}")
+    public ResponseEntity<List<User>> findByEmail(@PathVariable("email") String email) {
+        try {
+            List<User> users = userRepository.findByEmail(email);
+            if (users.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(users, HttpStatus.OK);
+
+            }
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUsersById(@PathVariable("id") long id) {
+        Optional<User> userData = userRepository.findById(id);
+        if (userData.isPresent()) {
+            return new ResponseEntity<>(userData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id , @RequestBody User user){
+        Optional<User> userData = userRepository.findById(id);
+        if (userData.isPresent()){
+            User _user = userData.get();
+            _user.setEmail(user.getEmail());
+            _user.setName(user.getName());
+            _user.setStatus(user.getStatus());
+            _user.setRole(user.getRole());
+            _user.setCreateDate(user.getCreateDate());
+            _user.setCreateUser(user.getCreateUser());
+
+            return new ResponseEntity<>(userRepository.save(_user) , HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/users")
+    public ResponseEntity<HttpStatus> deleteAllUsers(){
+        try {
+            userRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") long id){
+        try {
+            userRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
