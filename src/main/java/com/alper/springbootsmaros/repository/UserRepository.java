d86aa11f680e3a6660caf109c33bbfca4f5655f2
alperylmz1 <107919253+alperylmz1@users.noreply.github.com>
2022-07-21 08:10:56 +0300

    Add files via upload
    User Repository eklendi.

---
+++ src/main/java/com/alper/springbootsmaros/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.alper.springbootsmaros.repository;
+
+import com.alper.springbootsmaros.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findAll();
+
+    @Query("from User where CAST(email as text) like CONCAT(:email, '%')")
+    List<User> findByEmail(String email);
+
+}
+
+
